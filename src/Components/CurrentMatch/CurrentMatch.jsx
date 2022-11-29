7973fe2ef99c56124e1726bbafd87b41df5debb6
Vitor <frete497@gmail.com>
2022-11-29 19:30:17 -0400

    Current Match

--- src/Components/CurrentMatch/CurrentMatch.jsx
+++ src/Components/CurrentMatch/CurrentMatch.jsx
@@ -1,29 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import { Container } from './CurrentMatchStyle';
+import { Container, Current } from './CurrentMatchStyle';
 import circulo from '../../assets/circulo.svg';
 import Match from '../NextMatch/Match';
+import { GetURL } from '../Helper/GetURL';
 
 const CurrentMatch = () => {
   const [dados, setDados] = useState();
 
-  /* async function FetchCurrent() {
-    const response = await fetch(
-      `${import.meta.env.VITE_BASE_URL}matches/current`,
-    );
+  async function FetchCurrent() {
+    const response = await fetch(`https://worldcupjson.net/matches/current`);
     const data = await response.json();
-    console.log(data);
     setDados(data);
   }
 
   useEffect(() => {
     FetchCurrent();
-  }, []); */
+  }, []);
 
   return (
     <Container>
       <h1>
         <img src={circulo} /> AGORA
       </h1>
+
+      <Current>
+        {dados && dados.length > 0 ? (
+          dados.map((curr) => (
+            <Match
+              key={curr.id}
+              Team1={curr.home_team.name}
+              Team2={curr.away_team.name}
+              LogoT1={GetURL(curr.home_team.country)}
+              LogoT2={GetURL(curr.away_team.country)}
+              Info={`${curr.home_team.goals} - ${curr.away_team.goals}`}
+            />
+          ))
+        ) : (
+          <h2>Não tem nem um jogo acontecendo agora.</h2>
+        )}
+      </Current>
     </Container>
   );
 };
--- src/Components/CurrentMatch/CurrentMatchStyle.jsx
+++ src/Components/CurrentMatch/CurrentMatchStyle.jsx
@@ -7,11 +7,26 @@ export const Container = styled.div`
 
   > h1 {
     padding-top: 100px;
-    padding-bottom: 48px;
+    padding-bottom: 28px;
 
     display: flex;
     justify-content: center;
     align-items: center;
     gap: 16px;
+  }
+`;
+
+export const Current = styled.section`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 10px;
+  > h2 {
+    text-align: center;
+    padding: 10px;
+    font-weight: 500;
+    background-color: ${({ theme }) => theme.colors.bgColor};
+    border-radius: 6px;
+    box-shadow: 3px 3px 3px rgba(60, 57, 57, 0.2);
   }
 `;
